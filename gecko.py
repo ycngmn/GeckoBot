c1ba4bae47155773cd039831ea0df79136bcf838
Niloy88 <41749571+Niloy88@users.noreply.github.com>
2021-04-18 17:53:18 +0600

    Version 0.1
    — Reduced code  massively.. ! 
    —  removed .txt file dependency..
    — More friendly + fast 🤞

--- gecko.py
+++ gecko.py
@@ -1,23 +1,16 @@
 # -*- coding: future_fstrings -*-
-import telepot
-import requests
-import time,os
+import telepo,requests,time,os
 from bs4 import BeautifulSoup
 
-# bot vars
 userid = "" #your telegram channel ID 
 token = "" # API token from @botfather 
 
 bot=telepot.Bot(token)
 default="https://www.ccmalaysia.com/wp-content/uploads/2018/04/DQmWJDVhm1dgKZHbk4K1Es6MzhF1tDHvgotxb9Fbkv3Y8tf-768x385.png" # Any default image link    
 
-exists=os.path.exists("gecko.txt")
-if not exists:
-	f = open("gecko.txt","w+")
-	f.close()
+data = []
 
-# Scrapping part   — coingecko
-while True:
+while True: # Main Part
 	res = requests.get('https://www.coingecko.com/en/news')
 	soup = BeautifulSoup(res.content, 'lxml')
 	posts = soup.find_all('div', class_ = "col-lg-4 featured")
@@ -40,102 +33,13 @@ while True:
 			subx = "\n"
 		sub = subx.replace('&rsquo;','')
 		text = f"🔥 <b>{title}</b> !\n\n<i>{sub}\n\n</i><b>📰 Source :</b> {url}"
-		
-		z = open('gecko.txt','r')
-		x = z.read()
-		listx = x.split("\n")
-		z.close()
-		
-		if title in listx:
+		if title in data:
 			pass
 		else:
-			f = open('gecko.txt','a')
-			f.write(title + '\n')
-			f.close()
-			try:
-				bot.sendPhoto(userid,img,caption=text ,parse_mode='HTML')
+			data.append(title)
+			try: # To ignore the Bad Request Error so the bot keeps running..
+			bot.sendPhoto(user,img,caption=text ,parse_mode='HTML')
 			except:
-				img = default
-				bot.sendPhoto(userid,img,caption=text ,parse_mode='HTML')
-			print("Message 1 Executed ! ")
-	
-	
-	for post in posts [1:2] :
-		title = post.find('h3').text  
-		url = post.find('a')['href']
-		header = {
-    "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.169 Safari/537.36" ,
-    'referer':'https://www.google.com/'
-		}
-		subres = requests.get(url,headers=header)
-		subl = BeautifulSoup(subres.content,'lxml')
-		try:
-			img = subl.find("meta",  property="og:image").get('content')
-		except:
-			img = default
-		try:
-			subx = subl.find('meta', {'name':'description'}).get('content')
-		except:
-			subx = "\n"
-	
-		sub = subx.replace('&rsquo;','')
-		text = f"🔥 <b>{title}</b> !\n\n<i>{sub}\n\n</i><b>📰 Source :</b> {url}"
-		
-		z = open('gecko.txt','r')
-		x = z.read()
-		listx = x.split("\n")
-		z.close()
-		
-		if title in listx:
-			pass
-		else:
-			f = open('gecko.txt','a')
-			f.write(title + '\n')
-			f.close()
-			
-			try:
-				bot.sendPhoto(userid,img,caption=text ,parse_mode='HTML')
-			except:
-				img = default
-				bot.sendPhoto(userid,img,caption=text ,parse_mode='HTML')
-			print("Message 2 Executed ! ")
-	
-	for post in posts [2:3] :
-		title = post.find('h3').text  
-		url = post.find('a')['href']
-		header = {
-    "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.169 Safari/537.36" ,
-    'referer':'https://www.google.com/'
-		}
-		subres = requests.get(url,headers=header)
-		subl = BeautifulSoup(subres.content,'lxml')
-		try:
-			img = subl.find("meta",  property="og:image").get('content')
-		except:
-			img = default 
-		try:
-			subx = subl.find('meta', {'name':'description'}).get('content')
-		except:
-			subx = "\n"
-			
-		sub = subx.replace('&rsquo;','')
-		text = f"🔥 <b>{title}</b> !\n\n<i>{sub}\n\n</i><b>📰 Source :</b> {url}"
-		
-		z = open('gecko.txt','r')
-		x = z.read()
-		listx = x.split("\n")
-		z.close()
-		
-		if title in listx:
-			pass
-		else:
-			f = open('gecko.txt','a')
-			f.write(title + '\n')
-			f.close()
-			try:
-				bot.sendPhoto(userid,img,caption=text ,parse_mode='HTML')
-			except:
-				img = default
-				bot.sendPhoto(userid,img,caption=text ,parse_mode='HTML')
-			print("Message 3 Executed ! ")
-time.sleep(60)
+				pass
+			if len(data) == 12000: # Prevent data from gettig very big ;)
+				data = data[:1000]
