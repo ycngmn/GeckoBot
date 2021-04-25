30d6eff9b290c054814975f992f32e83ebfe2754
Niloy88 <41749571+Niloy88@users.noreply.github.com>
2021-04-25 06:53:12 +0600

    Add files via upload
    Fixed some issues from recent experiences ...  :)

--- gecko.py
+++ gecko.py
@@ -1,19 +1,19 @@
-import telepot,requests,time,os
+import telepot,requests,time
 from bs4 import BeautifulSoup
 
-user = "" #your telegram channel username 
+user = "@NewsGecko" #your telegram channel username
 token = "" # API token from @botfather 
 
 bot=telepot.Bot(token)
-default="https://www.ccmalaysia.com/wp-content/uploads/2018/04/DQmWJDVhm1dgKZHbk4K1Es6MzhF1tDHvgotxb9Fbkv3Y8tf-768x385.png" # Any default image link    
+default="https://example.c/ex.png" # Any image link to use if no image found in post..   
 
 data = []
 
-while True: # Main Part
+while True: # Main Part Begins
 	res = requests.get('https://www.coingecko.com/en/news')
 	soup = BeautifulSoup(res.content, 'lxml')
 	posts = soup.find_all('div', class_ = "col-lg-4 featured")
-	for post in posts [:1] :
+	for post in posts:
 		title = post.find('h3').text  
 		url = post.find('a')['href']
 		header = {
@@ -29,16 +29,19 @@ while True: # Main Part
 		try:
 			subx = subl.find('meta', {'name':'description'}).get('content')
 		except: 
-			subx = "\n"
+			subx = "( Unable to get the description)"
 		sub = subx.replace('&rsquo;','')
 		text = f"🔥 <b>{title}</b> !\n\n<i>{sub}\n\n</i><b>📰 Source :</b> {url}"
+		if title.endswith('?'):
+			text = f"🔥 <b>{title}</b> \n\n<i>{sub}\n\n</i><b>📰 Source :</b> {url}"
+
 		if title in data:
 			pass
 		else:
 			data.append(title)
-			try:  #ignores Bad Request Error...
+			try: # Mostly Except bad requests error.. 
 				bot.sendPhoto(user,img,caption=text ,parse_mode='HTML')
-			except:
-				pass
-			if len(data) == 12000: # Prevents data gettig too big ;)
-				data = data[:1000]
+			except Exception as e:
+				print(e)
+		if len(data) > 20: # to keep data's data in limit XD
+			data = data[-5:]
